0cda0c26b7b510445d68a324a96cb5ea10acc131
Adrian <adriansaleh061@gmail.com>
2024-12-14 21:35:41 +0100

    carousel done

--- back-end/server.js
+++ back-end/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-
+const Sport = require('../shared/classes/sport.js');
 const app = express();
 const PORT = 3000;
+
+// API call settings
 const wikidataEndPoint = 'https://query.wikidata.org/sparql';
+const headers = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-GB,en;q=0.9',
+    'Connection': 'keep-alive',
+    'Cookie': 'GeoIP=FR:ARA:Villeurbanne:45.77:4.89:v4; WMF-Last-Access-Global=14-Dec-2024',
+    'Host': 'query.wikidata.org',
+    'Sec-Fetch-Dest': 'document',
+    'Sec-Fetch-Mode': 'navigate',
+    'Sec-Fetch-Site': 'none',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.6 Safari/605.1.15'
+};
 
 // Middleware
 app.use(cors()); // Enable CORS
@@ -51,8 +65,8 @@ app.get('/carouselSearch', async (req, res) => {
     let query = ""; 
     // sparql query
     if(sports.includes(sport)) {
-        let randomOffset = Math.floor(Math.random() * 10); // random offset to get random results
         query = `SELECT ?label ?image ?inception ?authorityLabel ?countryOfOriginLabel ?icon ?unicode ?maxPlayers ?minPlayers (COUNT(?property) AS ?propertyCount) WHERE {
+    
         # Find the entity by label
         ?entity rdfs:label "${sport}"@en.
 
@@ -92,19 +106,14 @@ app.get('/carouselSearch', async (req, res) => {
         GROUP BY ?entity ?label ?image ?inception ?authorityLabel 
                 ?countryOfOriginLabel ?icon ?unicode ?maxPlayers ?minPlayers
         ORDER BY DESC(?propertyCount)
-        LIMIT 1
-        `;
-        console.log(query);
+        LIMIT 1`;
     } else {
         res.status(400).send('Invalid sport');
         return;
     }
     // sending a request to the database
     const url = `${wikidataEndPoint}?query=${encodeURIComponent(query)}&format=json`;
-    const response = await fetch(url, {
-        'User-Agent': 'Athletix/1.0 (adrian.abi-saleh@insa-lyon.fr)' // Add your app name and contact info
-    });
-    console.log('response', response);
+    const response = await fetch(url, { method: 'GET', headers });
     if (!response.ok) {
         res.status(500).send('Failed to fetch data');
         console.log('Failed to fetch data');
@@ -112,7 +121,21 @@ app.get('/carouselSearch', async (req, res) => {
     }
 
     const data = await response.json(); 
-    console.log('response', data);
+    let binding = data.results.bindings[0];
+
+    let sportObject = new Sport(
+        'label' in binding ? binding.label.value : null,
+        'image' in binding ? binding.image.value : null,
+        'inception' in binding ? binding.inception.value : null,
+        'authorityLabel' in binding ? binding.authorityLabel.value : null,
+        'countryOfOriginLabel' in binding ? binding.countryOfOriginLabel.value : null,
+        'icon' in binding ? binding.icon.value : null,
+        'unicode' in binding ? binding.unicode.value : null,
+        'maxPlayers' in binding ? binding.maxPlayers.value : null,
+        'minPlayers' in binding ? binding.minPlayers.value : null
+    );
+
+    res.send(sportObject);
 });
 
 
@@ -127,10 +150,6 @@ app.get('/searchBar', async (req, res) => {
             rdfs:label "${input}"@fr.
         }`;
 });
-
-
-
-
 
 // TODO list
 // 1. create api calls that work for the carousel 

90f05bdee0176a2d90fdebdf2a001830de60d637
gmarchimekari <gmarchimekari@gmail.com>
2024-12-16 17:02:41 +0100

    html update

--- front-end/src/app/components/athlete-page/athlete-page.component.ts
+++ front-end/src/app/components/athlete-page/athlete-page.component.ts
@@ -18,4 +18,15 @@ export class AthletePageComponent implements OnInit {
       this.player = data;
     });
   }
+
+  calculateAge(birthDate: any): number {
+    const birth = new Date(birthDate);
+    const today = new Date();
+    let age = today.getFullYear() - birth.getFullYear();
+    const monthDiff = today.getMonth() - birth.getMonth();
+    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birth.getDate())) {
+      age--;
+    }
+    return age;
+  }
 }
